4e0dd6486fa52e97ab9074cdb1fa8d4089c5bbb2
crisgorrino <cgorrino@gmail.com>
2014-05-20 12:19:23 -0500

    seccion de torre m

--- js/general.js
+++ js/general.js
@@ -1,3 +1,14 @@
-$(function(){
-    $(".over-txt"+(new Date().getTime() % 5 +1)).css("display", "block");
-});
+$(document).ready(function() {
+			
+			$('#fullpage').fullpage({
+			  //slidesColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+			  anchors: ['inicio', 'mazatlan', 'torreeme', 'plantas', 'amenidades','contacto','lastPage'],
+			  menu: '.menu',
+			  continuousVertical: false,
+			  controlArrowColor:'#85cfce'
+			});			
+			 //random text display
+			 $(".over-txt"+(new Date().getTime() % 5 +1)).css("display", "block");
+		});
+
+
