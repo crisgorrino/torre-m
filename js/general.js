5b7fe9cee644fd46bb7f83a7bce363709c9904e7
crisgorrino <cgorrino@gmail.com>
2014-05-22 13:21:06 -0500

    detalle de planta

--- js/general.js
+++ js/general.js
@@ -7,8 +7,21 @@ $(document).ready(function() {
 			  continuousVertical: false,
 			  controlArrowColor:'#85cfce'
 			});			
+			
 			 //random text display
 			 $(".over-txt"+(new Date().getTime() % 5 +1)).css("display", "block");
-		});
+			 
+			 //plantas detalle container toggle
+			 $('.recamara-num-btn').click(function(e){
+			 	 e.preventDefault();
+				 $('.plantas-info-container').css('height','100%').addClass('active'); 
+			 })
+			 
+			 $('.cerrar-planta-container').click(function(e){
+			 	 e.preventDefault();
+				 $('.plantas-info-container').css('height','0').removeClass('active');
+			 })
+			 
+});
 
 
