af1b9a8d5b39605ad9335d3eeefcf842b9c431f9
flavio <flavio@flavio-desktop.(none)>
2014-06-02 12:18:11 -0500

    pasar recamaras a archivo json

--- js/angularjs/recamaras_controller.js
+++ js/angularjs/recamaras_controller.js
@@ -6,158 +6,12 @@ var HomeApp = angular.module('HomeApp', [],  function($interpolateProvider) {
 
 
 HomeApp.controller('RecamarasCtrl', function ($scope, $http) {
-	$scope.recamaras = [
-	    {"recamaras": 1, 
-	    	"tipos": [
-	    		{
-	    			"order":1,
-	    			"modelo":"A", 
-	    			"descripcion": "1 recámara, sala, comedor, cocina, 1 baño completo, 1 centro de lavado y terraza.",
-	    			"m2":"79",
-	    			"img":"tipo-a.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":2,
-	    			"modelo":"B",
-	    			"descripcion": "1 recámara, sala, comedor, cocina, 1 baño completo, 1 centro de lavado, 1 bodega y terraza.",
-	    			"m2":"82",
-	    			"img":"tipo-b.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":3,
-	    			"modelo":"D",
-	    			"descripcion" : "1 recámara, sala, comedor, cocina, 1 baño completo, 1 centro de lavado y terraza.",
-	    			"m2":"83",
-	    			"img":"tipo-d.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    	]
-	    },
-	    {"recamaras": 2, 
-	    	"tipos": [
-	    		{
-	    			"order":1,
-	    			"modelo":"C",
-	    			"descripcion": "2 recámaras, sala, comedor, cocina, 1 baño completo, 1 centro de lavado y terraza.",
-	    			"m2": "82",
-	    			"img":"tipo-c.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":2,
-	    			"modelo":"E",
-    				"descripcion": "2 recámaras, sala, comedor, cocina, 1 baño completo, 1 centro de lavado y terraza.",
-	    			"m2": "82",
-	    			"img":"tipo-c.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":3,
-	    			"modelo":"F",
-    				"descripcion": "2 recámaras, sala, comedor, cocina, 1 baño completo, 1 centro de lavado y terraza.",
-	    			"m2": "82",
-	    			"img":"tipo-c.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":4,
-	    			"modelo":"G",
-    				"descripcion": "2 recámaras, sala, comedor, cocina, 1 baño completo, 1 centro de lavado y terraza.",
-	    			"m2": "82",
-	    			"img":"tipo-c.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":5,
-	    			"modelo":"H",
-    				"descripcion": "2 recámaras, sala, comedor, cocina, 1 baño completo, 1 centro de lavado y terraza.",
-	    			"m2": "82",
-	    			"img":"tipo-c.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":6,
-	    			"modelo":"I",
-    				"descripcion": "2 recámaras, sala, comedor, cocina, 1 baño completo, 1 centro de lavado y terraza.",
-	    			"m2": "82",
-	    			"img":"tipo-c.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":7,
-	    			"modelo":"I2",
-    				"descripcion": "2 recámaras, sala, comedor, cocina, 1 baño completo, 1 centro de lavado y terraza.",
-	    			"m2": "82",
-	    			"img":"tipo-c.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":8,
-	    			"modelo":"J",
-    				"descripcion": "2 recámaras, sala, comedor, cocina, 1 baño completo, 1 centro de lavado y terraza.",
-	    			"m2": "82",
-	    			"img":"tipo-c.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":9,
-	    			"modelo":"K",
-    				"descripcion": "2 recámaras, sala, comedor, cocina, 1 baño completo, 1 centro de lavado y terraza.",
-	    			"m2": "82",
-	    			"img":"tipo-c.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    	]
-		},
-	    {"recamaras": 3,
-	    	"tipos": [
-	    		{
-	    			"order":1,
-	    			"modelo":"L",
-	    			"descripcion":"3 recámaras, 1 cuarto de servicio con baño, sala, comedor, cocina, sala tv, área de bar, 3 baños completos, 1 medio baño, 1 centro de lavado, 1 bodega y terraza",
-	    			"m2":"458",
-	    			"img":"tipo-p.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":2,
-	    			"modelo":"M",
-	    			"descripcion":"3 recámaras, 1 cuarto de servicio con baño, sala, comedor, cocina, sala tv, área de bar, 3 baños completos, 1 medio baño, 1 centro de lavado, 1 bodega y terraza",
-	    			"m2":"458",
-	    			"img":"tipo-p.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":4,
-	    			"modelo":"N",
-	    			"descripcion":"3 recámaras, 1 cuarto de servicio con baño, sala, comedor, cocina, sala tv, área de bar, 3 baños completos, 1 medio baño, 1 centro de lavado, 1 bodega y terraza",
-	    			"m2":"458",
-	    			"img":"tipo-p.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":5,
-	    			"modelo":"O",
-	    			"descripcion":"3 recámaras, 1 cuarto de servicio con baño, sala, comedor, cocina, sala tv, área de bar, 3 baños completos, 1 medio baño, 1 centro de lavado, 1 bodega y terraza",
-	    			"m2":"458",
-	    			"img":"tipo-p.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    		{
-	    			"order":6,
-	    			"modelo":"P",
-	    			"descripcion":"3 recámaras, 1 cuarto de servicio con baño, sala, comedor, cocina, sala tv, área de bar, 3 baños completos, 1 medio baño, 1 centro de lavado, 1 bodega y terraza",
-	    			"m2":"458",
-	    			"img":"tipo-p.jpg",
-	    			"pdf":"118.pdf"
-	    		},
-	    	]
-		},
-  	];
-
-  	console.log($scope.recamaras);
+	//$scope.recamaras = $http.get('js/angularjs/data/recamaras.json');
+	
+	$http.get('js/angularjs/data/recamaras.json')
+   	.then(function(res){
+      $scope.recamaras = res.data;                
+    });
 
 
   	$scope.tipos = [];
@@ -172,7 +26,6 @@ HomeApp.controller('RecamarasCtrl', function ($scope, $http) {
 
   	$scope.showModelInfo = function(modelo){
   		$scope.modelo = modelo;
-  		console.log($scope.modelo);
   	};
 
   	$scope.showNext = function(){
